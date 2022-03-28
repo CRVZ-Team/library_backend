6c7a8cc63df1de8c32e6754e7f0411d703fe5812
Catalina Vartic <vartic.cata@gmail.com>
2022-03-29 01:10:24 +0200

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -9,14 +9,14 @@ def send_email(email, verificationString, type):
             from_email=os.environ.get('SENDGRID_EMAIL'),
             to_emails=email,
             subject='Verify your email',
-            html_content=f'Thank you for signing up! Click here to <a href="http://localhost:3000/api/verify-email?verificationString={verificationString}">verify your email</a>')
+            html_content=f'Thank you for signing up! Click here to <a href="http://localhost:3000/verify-email/{verificationString}">verify your email</a>')
     
     elif type == 'reset':
         message = Mail(
             from_email=os.environ.get('SENDGRID_EMAIL'),
             to_emails=email,
             subject='Reset your password',
-            html_content=f'Click here to <a href="http://localhost:3000/api/users/{verificationString}/reset-password">reset your password</a>')
+            html_content=f'Click here to <a href="http://localhost:3000/reset-password/{verificationString}">reset your password</a>')
 
     try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'),)
