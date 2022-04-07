e05172ee6e97afacc63cc17c3bb4c468cbaf53f0
Catalina Vartic <vartic.cata@gmail.com>
2022-04-07 13:44:58 +0200

    Refactoring app.py with try, except, and finally

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ Base = declarative_base()
 engine = create_engine(os.getenv("DATABASE_URL"))
 create_session = sessionmaker(bind=engine)
 
+# Create the SQLAlchemy pluggin connected to the database
 plugin = sqlalchemy.Plugin(
     engine,
     Base.metadata,
@@ -37,6 +38,7 @@ def default_json(t):
         return "{:.2f}".format(t)
     return f'{t}'
 
+# Handle CORS policy for the frontend
 class EnableCors(object):
     name = 'enable_cors'
     api = 2
@@ -62,19 +64,18 @@ secret = os.environ.get('JWT_SECRET')
 def index():
     return 
 
+# LOGIN
 @route('/api/login', method=['OPTIONS', 'POST'])
 def _():
     try:
         payload = json.loads(request.body.read())
         email = payload['email']
-        print(email)
         password = payload['password']
-        print(password)
+
         session = create_session()
         user = session.query(User).filter_by(email=email).first()
-        print(user)
+
         if not user:
-            print("User not found")
             response.status = 401
             return response
         
@@ -82,74 +83,93 @@ def _():
         if not isCorrect:
             response.status = 401
             return response
+        
         # add more variables token
-        token = jwt.encode({'id': user.id}, secret, algorithm='HS256')
+        token = jwt.encode({'id': user.id, 'email': user.email, 'verified': user.is_verified, 'admin': user.is_admin}, secret, algorithm='HS256')
         if not token:
             response.status = 401
             return response
+        
         response.status = 200
         return {'token': token}
     except Exception as e:
         return {'status': 'error', 'message': 'Invalid email or password'}
+    finally:
+        session.close()
 
+# Sign up
 @route('/api/signup', method=['OPTIONS', 'POST'])
 def _():
     try:
         payload = json.loads(request.body.read())
         email = payload['email']
-        print(email)
         password = payload['password']
-        print(password)
+
         session = create_session()
         user = session.query(User).filter_by(email=email).first()
         if user:
             response.status = 401
             return response
+
         verificationString = str(uuid4())
         passwordHash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         user = User(email, passwordHash, verificationString)
-        print(user)
         session.add(user)
         session.commit()
 
         send_email(email, verificationString, 'verify')
+
         # add more variables token
-        token = jwt.encode({'id': user.id}, secret, algorithm='HS256')
+        token = jwt.encode({'id': user.id, 'email': user.email, 'verified': user.is_verified, 'admin': user.is_admin}, secret, algorithm='HS256')
         response.status = 200
         return {'token': token}
     except Exception as e:
-        print(e)
         response.status = 500
         return {'message': 'Error sending email'}
+    finally:
+        session.close()
 
+
+# Verify email
 @route('/api/verify-email', method=['OPTIONS', 'PUT'])
 def _():
-    payload = json.loads(request.body.read())
-    verificationString = payload['verificationString']
-    session = create_session()
-    user = session.query(User).filter_by(verification_string=verificationString).first()
-    
-    if not user:
-        response.status = 401
-        return {'message': 'Invalid verification string'}
-    
-    user.is_verified = True
-    user.verification_string = ''
-    session.commit()
+    try:
+        payload = json.loads(request.body.read())
+        verificationString = payload['verificationString']
 
-    token = jwt.encode({'id': user.id}, secret, algorithm='HS256')  
+        session = create_session()
+        user = session.query(User).filter_by(verification_string=verificationString).first()
+        
+        if not user:
+            response.status = 401
+            return {'message': 'Invalid verification string'}
+        
+        user.is_verified = True
+        user.verification_string = ''
+        session.commit()
 
-    if not token:
+        token = jwt.encode({'id': user.id, 'email': user.email, 'verified': user.is_verified, 'admin': user.is_admin}, secret, algorithm='HS256')  
+
+        if not token:
+            response.status = 500
+            return response
+
+        response.status = 200
+        return response
+    except Exception as e:
         response.status = 500
         return response
-    response.status = 200
-    return response
+    finally:
+        session.close()
 
+
+# Reset the password
 @route('/api/users/<passwordResetCode>/reset-password', method=['OPTIONS', 'PUT'])
 def _(passwordResetCode):
     try:
         payload = json.loads(request.body.read())
         newPassword = payload['newPassword']
+
         session = create_session()
         user = session.query(User).filter_by(password_reset_code=passwordResetCode).first()
         if not user:
@@ -159,13 +179,17 @@ def _(passwordResetCode):
         user.password = bcrypt.hashpw(newPassword.encode('utf-8'), bcrypt.gensalt())
         user.password_reset_code = ''
         session.commit()
+
         response.status = 200
         return response
     except Exception as e:
-        print(e)
         response.status = 500
         return {'message': 'Error resetting password'}
+    finally:
+        session.close()
 
+
+# Forgot password
 @route('/api/forgot-password/<email>', method=['OPTIONS', 'PUT'])
 def _(email):
     try:
@@ -174,51 +198,73 @@ def _(email):
         if not user:
             response.status = 401
             return {'message': 'Invalid email'}
+
         passwordResetCode = str(uuid4())
         user.password_reset_code = passwordResetCode
         session.commit()
+
         send_email(email, passwordResetCode, 'reset')
+
         response.status = 200
         return response
 
     except Exception as e:
         response.status = 500
         return response
+    finally:
+        session.close()
 
 
 @route('/api/users/<id>', method=['OPTIONS', 'GET'])
 
 
-@route('/api/book/<id>', method=['OPTIONS', 'GET'])
+# Single book page
+@route('/api/books/<id>', method=['OPTIONS', 'GET'])
 def _(id):
-    session = create_session()
-    book = session.query(Book).filter_by(id=id).first()
-    if not book:
-        response.status = 404
+    try:
+        session = create_session()
+        book = session.query(Book).filter_by(id=id).first()
+        if not book:
+            response.status = 404
+            return response
+
+        # get genres for the book\
+        genre_ids = session.query(Genre).join(BookGenre).filter_by(book_id=id).all()
+        genres = ", ".join([genre.type for genre in genre_ids])
+
+        # get reviews for the book
+        reviews_query = session.query(Review, User.email).filter(Review.user_id == User.id).filter(Review.book_id==id).filter(Review.approved == True).all()
+        reviews	= [{'comment': review[0].to_dict(), 'user': review[1]} for review in reviews_query]
+
+        # get coeficient for the book price
+        coeficient = session.query(Coeficient).first()
+
+        #get subscribers for the book
+        subscriptions = {"month": book.price * coeficient.month, "year": book.price * coeficient.year}
+
+        data = {'book': book.to_dict(), 'genres': genres, 'subscriptions': subscriptions, 'reviews': reviews}
+        response.status = 200
+        return json.dumps(data, default=default_json)
+    except Exception as e:
+        response.status = 500
         return response
-    # get genres for the book\
-    genre_ids = session.query(Genre).join(BookGenre).filter_by(book_id=id).all()
-    genres = ", ".join([genre.type for genre in genre_ids])
-    # get reviews for the book
-    reviews_query = session.query(Review, User.email).filter(Review.user_id == User.id).filter(Review.book_id==id).filter(Review.approved == True).all()
-    reviews	= [{'comment': review[0].to_dict(), 'user': review[1]} for review in reviews_query]
-    # get coeficient for the book price
-    coeficient = session.query(Coeficient).first()
-    subscriptions = {"month": book.price * coeficient.month, "year": book.price * coeficient.year}
-    data = {'book': book.to_dict(), 'genres': genres, 'subscriptions': subscriptions, 'reviews': reviews}
-    session.close()
-    response.status = 200
-    return json.dumps(data, default=default_json)
+    finally:
+        session.close()
 
 # Route for getting the books
 @route('/api/books', method=['OPTIONS', 'GET'])
 def _():
-    session = create_session()
-    books = session.query(Book).all()
-    print(books)
-    session.close()
-    response.status = 200
-    return json.dumps([book.to_dict() for book in books], default=default_json)
+    try:
+        session = create_session()
+        books = session.query(Book).all()
+
+        response.status = 200
+        return json.dumps([book.to_dict() for book in books], default=default_json)
+    except Exception as e:
+        response.status = 500
+        return response
+    finally:
+        session.close()
 
 
 # FEATURE for ADMIN (add book)
@@ -230,18 +276,26 @@ def _():
         author = payload['author']
         description = payload['description']
         image_url = payload['image_url']
+        price = payload['price']
+        genres = payload['genres']
+    
         session = create_session()
-        book = Book(title, author, description, image_url)
+        book = Book(title, author, description, image_url, price)
+        # genres
+
         session.add(book)
         session.commit()
+
         response.status = 201
         return response
     except Exception as e:
         response.status = 500
         return {'message': 'Error creating book'}
+    finally:
+        session.close()
 
 # FEATURE for ADMIN (delete book)
-@route('/api/book/<id>', method=['OPTIONS', 'DELETE'])
+@route('/api/books/<id>', method=['OPTIONS', 'DELETE'])
 def _(id):
     session = create_session()
     book = session.query(Book).filter_by(id=id).first()
@@ -253,7 +307,7 @@ def _(id):
     response.status = 200
     return response
 
-@route('/api/book/<id>', method=['OPTIONS', 'PUT'])
+@route('/api/books/<id>', method=['OPTIONS', 'PUT'])
 def _(id):
     try:
         payload = json.loads(request.body.read())
@@ -277,6 +331,8 @@ def _(id):
         response.status = 500
         return {'message': 'Error updating book'}
 
+
+# 
 @route('/api/users/<id>', method=['OPTIONS', 'GET'])
 def _(id):
     session = create_session()
@@ -288,7 +344,7 @@ def _(id):
     return user.to_dict()
 
 
-# Route 
+# Reviews and omments for the book
 @route('/api/comment', method=['OPTIONS', 'POST'])
 def _():
     try:
@@ -298,9 +354,11 @@ def _():
         comment = payload['comment']
         book_id = payload['book_id']
         user_id = payload['user_id']
+
         review = Review(book_id=book_id, user_id=user_id, comment=comment, rating=rating, approved=False)
         session.add(review)
         session.commit()
+
         response.status = 201
         return response
     except Exception as e:
@@ -310,30 +368,35 @@ def _():
         session.close()
 
     
+# Filters for the books
 @route('/api/filters/authors', method=['OPTIONS', 'GET'])
 def _():
-    session = create_session()
+    try:
+        session = create_session()
 
-    #query authors
-    authors_data = session.query(Book).all()
-    authors = [{'name': book.author} for book in authors_data]
+        #query authors
+        authors_data = session.query(Book).all()
+        authors = [{'name': book.author} for book in authors_data]
 
-    #query year span
-    max_year = session.query(func.max(Book.year)).first()[0]
-    min_year = session.query(func.min(Book.year)).first()[0]
+        #query year span
+        max_year = session.query(func.max(Book.year)).first()[0]
+        min_year = session.query(func.min(Book.year)).first()[0]
 
-    year_span = [{"max": max_year, "min": min_year}]
+        year_span = [{"max": max_year, "min": min_year}]
 
-    #query genres
-    genres_data = session.query(Genre).all()
-    genres = [{'name': genre.type} for genre in genres_data]
+        #query genres
+        genres_data = session.query(Genre).all()
+        genres = [{'name': genre.type} for genre in genres_data]
 
-    #create general dict
-    data =  [{"authors": authors}, {"year_span": year_span}, {"genres": genres}]
-    response.status = 200
-    session.close()
-    return json.dumps(data, default=default_json)
-
+        #create general dict
+        data =  [{"authors": authors}, {"year_span": year_span}, {"genres": genres}]
+        response.status = 200
+        return json.dumps(data, default=default_json)
+    except Exception as e:
+        response.status = 500
+        return response
+    finally:
+        session.close()
 
 if os.environ.get('APP_LOCATION') == 'heroku':
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
